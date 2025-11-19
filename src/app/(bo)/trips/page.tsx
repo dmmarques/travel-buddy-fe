123f78fd67971c799bc9061245012104dd1678a0
Diogo Marques VDF <diogo.marques@vodafone.com>
2025-11-19 01:33:01 +0000

    Fixing last base url

--- src/app/(bo)/trips/page.tsx
+++ src/app/(bo)/trips/page.tsx
@@ -29,7 +29,7 @@ import type { Trip } from "./types/trip";
 
 const BASE_URL =
   process.env.TRAVEL_API_BASE_URL ||
-  "https://travel-management-fs-production.up.railway.app";
+  "https://travel-management-fs-production.up.railway.app/travel-management-ms";
 
 export default function TripsDashboard() {
   const router = useRouter();
