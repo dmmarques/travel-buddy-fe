9a354284c6ba9af719c48d7c7c7bad1e962a5c76
Diogo Marques VDF <diogo.marques@vodafone.com>
2025-12-05 09:03:47 +0000

    Replacing ANY by the specific types

--- src/app/utilies/api/weather.ts
+++ src/app/utilies/api/weather.ts
@@ -1,8 +1,22 @@
+export interface DailyWeather {
+  date: string;
+  weatherCode: number;
+  minTemperature: number;
+  maxTemperature: number;
+  weatherStatus: string;
+}
+
+export interface WeatherStatusResponse {
+  latitude: number;
+  longitude: number;
+  dailyWeather: DailyWeather[];
+}
+
 export async function getWeatherStatus(
   latitude: number,
   longitude: number,
   dates: string[]
-): Promise<any> {
+): Promise<WeatherStatusResponse> {
   const dateParam = dates.join(",");
   const res = await fetch(
     `http://localhost:8082/weather/check?latitude=${latitude}&longitude=${longitude}&dates=${dateParam}`
